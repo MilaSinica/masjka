452b0cff0bf3135d3547985a8f3b452aefc14279
MilaSinica <ludmila.sinica@gmail.com>
2016-02-14 14:14:25 +0200

    Angry dog, facebook share, nav improvements

--- assets/js/external.js
+++ assets/js/external.js
@@ -28,23 +28,56 @@
   		document.getElementById('time-button').innerHTML = Date();
 	}	
 
-	function setDate(){
+	function setDate() {
   		document.getElementById('date').innerHTML = Date();
+  		var dog = document.getElementById('angry');
+  		var neutralDog = true;
+  		setInterval(function() {
+  			if(neutralDog) {
+  				dog.className = "angry-dog angry-dog-happy";
+  				neutralDog = false;
+  			} else {
+  				dog.className = "angry-dog angry-dog-neutral";
+  				neutralDog = true;
+  			}
+
+  		}, 1000);
 	}
 
 	function message2(msg) {
   		document.getElementById('scroll').innerHTML = msg;
 	}
 
-	function closeMe(){
+	function closeMe() {
    		x=document.getElementById("cens");
    		x.style.display="none";
 	}
 
-	function openMe(){
+	function openMe() {
    		x=document.getElementById("cens");
 		x.style.display="block";
 	}
+
+	function onAngryDogClick(className) {
+		document.getElementById('angry').className = "angry-dog " + className;
+	}
+
+/*var navTextOpacity;
+var navImageOpacity;
+	function navOpacity(navItem) {
+		var navText = navItem.getElementsByClassName('nav-text')[0];
+		var navImage = navItem.getElementsByClassName('nav-img')[0];
+		navTextOpacity = navText.style.opacity;
+		navImageOpacity = navImage.style.opacity;
+
+		navText.style.opacity="0";
+		navImage.style.opacity="1";
+	}
+
+	function navBack(navItem) {
+		navItem.getElementsByClassName('nav-text')[0].style.opacity = navTextOpacity;
+		navItem.getElementsByClassName('nav-img')[0].style.opacity = navImageOpacity;
+	}*/
 
 	/*function openMe(){
    		x=document.getElementById("cens2");
